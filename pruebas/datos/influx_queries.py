56969823c38034b543b50dc7f55f4f4d10f3b51f
RodrigoGM97 <rodrigogm971@gmail.com>
2019-05-02 21:32:20 -0500

    hola

--- pruebas/datos/influx_queries.py
+++ pruebas/datos/influx_queries.py
@@ -7,6 +7,15 @@ def clean_json(list_json):
 		x = json.loads(x)
 	return list_json
 
+def clean_time(date, hour):
+	hour = clean_hour(hour)
+	time_str = "'"+date+"T"+hour+":00Z"
+	return time_str
+
+def clean_hour(hour):
+	newhour = hour[0] + hour[1] + hour[2] + "00"
+	return newhour
+
 client = InfluxDBClient(host='127.0.0.1', port=8086, database='pfprueba')
 client.switch_database('pfprueba')
 measurements = client.get_list_measurements()
@@ -25,7 +34,8 @@ for i in range(len(query)):
 		print (key, value)
 
 print("Time")
-print(query[0]["time"])
+print(query[0])
+
 
 print("Query")
 estados = client.query('show tag values on "pfprueba" from "Temperatura" with key="estado"')
@@ -49,3 +59,15 @@ get_time_estado = client.query(get_time_estado_str)
 get_time_estado = list(get_time_estado.get_points())
 get_time_estado = clean_json(get_time_estado)
 print(get_time_estado[0]["clima"])
+
+listStates = []
+for i in range(len(estados)):
+	listStates.append(estados[i]['value'])
+
+print(listStates)
+
+date="2019-05-16"
+hour="14:20"
+newdate = clean_time(date, hour)
+print(newdate)
+
