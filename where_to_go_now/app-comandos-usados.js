e0180cbe258d8108713b90f8b6a168d71b0a83ae
saulelabra <quiquelabra@gmail.com>
2019-05-09 10:42:49 -0500

    actualizacion de archivos de comandos y adicion de imagenes

--- where_to_go_now/app-comandos-usados.js
+++ where_to_go_now/app-comandos-usados.js
@@ -3,7 +3,7 @@
 2. Cámbiese a la carpeta del proyecto.
 3. Compile la imagen personalizada de la aplicación:
 
-docker build -t flask-mongo .
+docker build -t flaskpf .
 
 4. Verifique que la imagen fue creada correctamente y que se encuentra en el repositorio local de imágenes:
 
@@ -11,7 +11,7 @@ docker build -t flask-mongo .
 
 7. Inicie un contenedor con la aplicación, a partir de la imagen generada:
 
-docker run --rm --name app -p 5000:5000 -d --net mongo-sh flask-mongo
+docker run --rm --name app -p 8080:8080 -d --net mongo-sh flaskpf
  
 8. Verifique que el contendor se encuentra en ejecución:
 
@@ -19,7 +19,7 @@ docker run --rm --name app -p 5000:5000 -d --net mongo-sh flask-mongo
 
 9. Acceda a la interfaz web de la aplicación en un browser en la URL:
 
-[http://localhost:5000](http://localhost:5000)
+[http://localhost:8080](http://localhost:8080)
 
 10. Detenga los contenedores:
 
